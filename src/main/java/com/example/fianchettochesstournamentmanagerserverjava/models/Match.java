8c3a0d9c42fb53e8bcdd355aec0ddb7f162b0ef6
Fenil Shah <fenilshah0112@gmail.com>
2020-04-05 21:37:36 -0400

    Define tables
    Defining all the tables for the database.

---
+++ src/main/java/com/example/fianchettochesstournamentmanagerserverjava/models/Match.java
@@ -0,0 +1,41 @@
+package com.example.fianchettochesstournamentmanagerserverjava.models;
+
+public class Match {
+	
+	private String id;
+	private User player1;
+	private User player2;
+	private int result;
+	private Round round;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public User getPlayer1() {
+		return player1;
+	}
+	public void setPlayer1(User player1) {
+		this.player1 = player1;
+	}
+	public User getPlayer2() {
+		return player2;
+	}
+	public void setPlayer2(User player2) {
+		this.player2 = player2;
+	}
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+	public Round getRound() {
+		return round;
+	}
+	public void setRound(Round round) {
+		this.round = round;
+	}
+}
